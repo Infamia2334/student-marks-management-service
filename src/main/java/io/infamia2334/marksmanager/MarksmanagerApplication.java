2d888719c2404424cdbbff9e8dcb3876bc95ace6
Dipan Das <dipanisboss@gmail.com>
2022-04-08 16:44:34 +0530

    Initialised new Spring Boot project

---
+++ src/main/java/io/infamia2334/marksmanager/MarksmanagerApplication.java
@@ -0,0 +1,13 @@
+package io.infamia2334.marksmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/infamia2334/marksmanager/MarksmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.infamia2334.marksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarksmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
