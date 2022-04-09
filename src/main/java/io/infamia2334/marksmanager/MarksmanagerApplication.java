ef5ba67ab79d72127608f23e95387452e8876252
Dipan Das <dipanisboss@gmail.com>
2022-04-09 20:18:06 +0530

    Added new configuration for excluding spring security and base url path

--- src/main/java/io/infamia2334/marksmanager/MarksmanagerApplication.java
+++ src/main/java/io/infamia2334/marksmanager/MarksmanagerApplication.java
@@ -2,8 +2,9 @@ package io.infamia2334.marksmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MarksmanagerApplication {
 
 	public static void main(String[] args) {
